55740acf3804a3001a1dde47380aca71dc8ecb87
sabari <sabarinathan.boopathi@theoptimum.tech>
2025-12-16 22:25:20 +0530

    fix: old arch support for android

--- android/src/main/java/com/securenetwork/SecureNetworkPackage.kt
+++ android/src/main/java/com/securenetwork/SecureNetworkPackage.kt
@@ -1,13 +1,13 @@
 package com.securenetwork
 
-import com.facebook.react.BaseReactPackage
+import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
 import java.util.HashMap
 
-class SecureNetworkPackage : BaseReactPackage() {
+class SecureNetworkPackage : TurboReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
     return if (name == SecureNetworkModule.NAME) {
       SecureNetworkModule(reactContext)
